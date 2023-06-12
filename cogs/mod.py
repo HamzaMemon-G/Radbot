1e4d729327e3428cead542cd1d9f5f917e4fe64f
Hamza Memon <hamzamemon7002@gmail.com>
2023-06-12 19:23:54 +0530

    seprated moderation commands

---
+++ cogs/mod.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from typing import Optional
+import asyncio
+from datetime import timedelta
+
+class Mod(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="kick", description="Kick a user from the server")
+    async def kick(self, interaction: discord.Interaction, user: discord.Member, *, reason: str):
+        await user.kick(reason=reason)
+        await interaction.channel.send(f"Kicked {user.mention} for **{reason}**")
+        await interaction.response.send_message(f"Kicked {user} for **{reason}**", ephemeral=True)
+    
+    @app_commands.command(name="warn", description="Warn a user")
+    async def warn(self, interaction: discord.Interaction, user: discord.Member, *, reason: str):
+        await interaction.channel.send(f"Warned {user.mention} for **{reason}**")
+        await interaction.response.send_message(f"Warned {user} for **{reason}**", ephemeral=True)
+
+    @app_commands.command(name="whois", description="Get information about a user")
+    async def whois(self, interaction: discord.Interaction, user: discord.Member):
+        embed = discord.Embed(title=f"{user}", description=f"Information about {user.mention}", color=user.color)
+        embed.add_field(name="ID", value=user.id, inline=False)
+        embed.add_field(name="Joined at", value=user.joined_at.strftime("%d/%m/%Y, %H:%M:%S"), inline=False)
+        embed.add_field(name="Created at", value=user.created_at.strftime("%d/%m/%Y, %H:%M:%S"), inline=False)
+        embed.set_thumbnail(url=user.display_avatar)
+        embed.set_footer(text=f"Requested by {interaction.user}")
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+    
+    @app_commands.command(name="purge", description="Purge messages in a channel")
+    async def purge(self, interaction: discord.Interaction, amount: int):
+        await interaction.channel.purge(limit=amount)
+        await interaction.response.send_message(f"Purged {amount} messages", ephemeral=True)
+
+    @app_commands.command(name="ban", description="Ban a user from the server")
+    async def ban(self, interaction: discord.Interaction, user: discord.Member, *, reason: str):
+        await user.ban(reason=reason)
+        await interaction.channel.send(f"Banned {user.mention} for **{reason}**")
+        await interaction.response.send_message(f"Banned {user} for **{reason}**", ephemeral=True)
+
+    @app_commands.command(name="unban", description="Unban a user from the server")
+    async def unban(self, interaction: discord.Interaction, user: discord.User, *, reason: str):
+        member = await bot.fetch_user(user.id)
+        await interaction.guild.unban(member, reason=reason)
+        await interaction.channel.send(f"Unbanned {user.mention} for **{reason}**")
+        dm_channel = await user.create_dm()
+        await dm_channel.send(f"You have been unbanned from **{interaction.guild.name}** for **{reason}**")
+    
+    @app_commands.command(name="timeout", description="Timeout a user")
+    async def timeout(self, interaction: discord.Interaction, member: discord.Member, minitues: int, *, reason: str):
+        delta = timedelta(minutes=minitues)
+        await member.timeout(delta, reason=reason)
+        await interaction.channel.send(f"Timed out {member.mention} for **{reason}**")
+        await interaction.response.send_message(f"Timed out {member} for **{reason}**", ephemeral=True)
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Mod(bot))
