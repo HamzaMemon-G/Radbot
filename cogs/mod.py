2a4f663925b4ccb5381c700d27d91cdc23fdfcd7
Hamza Memon <hamzamemon7002@gmail.com>
2023-06-12 20:09:51 +0530

    Removed unneccessary libraries

--- cogs/mod.py
+++ cogs/mod.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 from typing import Optional
-import asyncio
 from datetime import timedelta
 
 class Mod(commands.Cog):
