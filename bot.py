6b3631062bdd6e4a379637a6e69d03c5d42f4b57
Hamza Memon <hamzamemon7002@gmail.com>
2023-06-13 23:18:47 +0530

    Added subclass

--- bot.py
+++ bot.py
@@ -1,38 +1,26 @@
 import discord
 import discord.ext.commands as commands
-from datetime import timedelta
 from dotenv import load_dotenv
 import os
-import random
-from typing import Optional
-import asyncio
 
 load_dotenv()
-
 token = os.getenv('BOT_TOKEN')
-
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
 
 cogs = ["cogs.utils", "cogs.mod"]
 
-@bot.event
-async def on_ready():
-    print('Bot is ready')
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix: str, intents: discord.Intents, **kwargs):
+        super().__init__(command_prefix, intents=intents, **kwargs)
     
-    for cog in cogs:
-        await bot.load_extension(cog)
-    print('Loaded all cogs')
+    async def setup_hook(self) -> None:
+        for cog in cogs:
+            await bot.load_extension(cog)
+        print("Loaded all cogs")
 
-    await bot.tree.sync()
+    async def on_ready(self):
+        print("Bot is ready")
 
-@bot.event
-async def on_message(msg: discord.Message):
+if __name__ == "__main__":
 
-    greetings = ["Hi", "Hello", "Hey", "Yo","What's sup"]
-    random_greeting = random.choice(greetings)
-    if msg.content.lower() in greetings:
-        await msg.channel.send(f'{random_greeting} {msg.author.mention}')
-
-
-bot.run(token)
+    bot = MyBot(command_prefix='!', intents=discord.Intents.all())
+    bot.run(token)
