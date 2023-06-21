2935063d65f1f3d3e73f72b9d743ecf84503f5e7
Hamza Memon <hamzamemon7002@gmail.com>
2023-06-21 13:18:32 +0530

    added list for commands

---
+++ cogs/extra.py
@@ -0,0 +1,65 @@
+allcommandslist = [
+        {
+            "name": "announce",
+            "description": "Command to announce a message to a channel"
+        },
+        {
+            "name": "poll",
+            "description": "Command to create a poll"
+        },
+        {
+            "name": "say",
+            "description": "Command to say a message"
+        },
+        {
+            "name": "status",
+            "description": "Command to check status of bot"
+        },
+        {
+            "name": "synccommand",
+            "description": "Command to sync all commands"
+        },
+        {
+            "name": "kick",
+            "description": "Command to kick member"
+        },
+        {
+            "name": "warn",
+            "description": "Command to warn member"
+        },
+        {
+            "name": "whois",
+            "description": "Command to get information about member"
+        },
+        {
+            "name": "purge",
+            "description": "Command to delete messages"
+        },
+        {
+            "name": "ban",
+            "description": "Command to ban member"
+        },
+        {
+            "name": "unban",
+            "description": "Command to unban member"
+        },
+        {
+            "name": "timeout",
+            "description": "Command to timeout member"
+        }
+    ]
+
+usercommandslist = [
+        {
+            "name": "help",
+            "description": "Command to get help about commands"
+
+        },
+        {
+            "name": "botinfo",
+            "description": "Command to get information about member"
+        }
+]
+
+url = "https://github.com/HamzaMemon-G/Radbot"
+iconurl = "https://i.imgur.com/Zn06dLv.png"
