d2627ea63e3d9b27be1eb60a8f5993053ca11f7a
Hamza Memon <hamzamemon7002@gmail.com>
2023-07-11 15:03:24 +0530

    changed content

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -9,14 +9,14 @@ class Welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        embed = discord.Embed(title = "Welcome👋", description=f"{member.mention} thank you for joining the Odyssey VTC Discord server!\n\n Feel free to look around and join our community.", color=discord.Color.from_rgb(255, 119, 0))
-        embed.add_field(name="> **Wanna join the Odyssey VTC?**", value="**You can apply [here](https://truckersmp.com/user/5132120)**")
-        embed.add_field(name="> **Have any questions?**", value=f"**Feel free to ask in {self.bot.get_channel(1128020624267280424).mention}**")
+        embed = discord.Embed(title = "Welcome👋", description=f"{member.mention} thank you for joining the RADAR'S SERVER Discord server!\n\n Feel free to look around and join our community.", color=discord.Color.from_rgb(255, 119, 0))
+        embed.add_field(name="> **Wanna join the RADAR'S SERVER?**", value=f"**You can get the ip here {self.bot.get_channel(1006098590025928755).mention} **")
+        embed.add_field(name="> **Have any questions?**", value=f"**Feel free to ask in {self.bot.get_channel(992123833203036270).mention}**")
         embed.set_thumbnail(url=member.avatar)
-        embed.set_image(url="https://i.imgur.com/oYhNR6h.png")
-        embed.set_footer(text="Odyssey VTC Staff", icon_url="https://i.imgur.com/YRjbLdD.png")
-        channel1 = self.bot.get_channel(1128017313908670464)
-        channel2 = self.bot.get_channel(1128020624267280424)
+        embed.set_image(url="https://i.imgur.com/zxnfIg2.png")
+        embed.set_footer(text="RADAR'S SERVER Staff", icon_url="https://i.imgur.com/zxnfIg2.png")
+        channel1 = self.bot.get_channel(980707987716997160)
+        channel2 = self.bot.get_channel(980385971466022915)
         await channel1.send(embed=embed)
         await channel2.send(embed=embed)
 
